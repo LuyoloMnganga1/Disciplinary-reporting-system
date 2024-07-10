1ecbebbf6a1debc72bb929d1eb0de61b2ba27435
Luyolo Mnganga <Luyolomnganga@gmail.com>
2024-07-10 15:45:11 +0200

    Update UserPasswordsSeeder.php

--- database/seeders/UserPasswordsSeeder.php
+++ database/seeders/UserPasswordsSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\UserPasswords;
+use Illuminate\Support\Facades\Hash;
 
 class UserPasswordsSeeder extends Seeder
 {
